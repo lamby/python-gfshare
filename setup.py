dfaa3ba96c01d0fa7bf84536e7f4f24569589951
Chris Lamb <chris@chris-lamb.co.uk>
2017-12-13 22:17:15 +0000

    Releasing version 1.0.8

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('README.rst') as f:
 
 setup(
     name='gfshare',
-    version='1.0.7',
+    version='1.0.8',
     description="Python interface to libgfshare",
     long_description=long_description,
 
